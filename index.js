054e32e751d6e94b47debf04035b86ef3f337ee5
guswlda <guswl71945@gmail.com>
2024-10-04 12:04:11 +0900

    user based recommend

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// dev
+// 개발 환경 확인 함수
 const isDevelopment = () => {
   return process.env.NODE_ENV == 'development';
 };
 
-// env 있을 경우 C, env 없는 경우 miniconda
+// 환경에 따라 파이썬 경로 설정
 const pythonExePath = isDevelopment()
   ? path.join('C:', 'conda', 'envs', 'recom_env', 'python.exe')
   : path.join('/home/ubuntu/miniconda', 'envs', 'myenv', 'bin', 'python3');
@@ -25,55 +25,43 @@ app.get('/', (req, res) => {
 });
 
 // Python 스크립트를 실행하는 함수 정의
-function executePythonScript(scriptName, args, res) {
+function executePythonScript(scriptName, args, res, inputData = null) {
   const scriptPath = path.join(__dirname, scriptName);
-  // const pythonPath = path.join(
-  //   'C:',
-  //   'conda',
-  //   'envs',
-  //   'recom_env',
-  //   'python.exe'
-  // );
-
-  // deploy pythonPath
-  // const pythonPath = path.join('__dirname', 'venv', 'bin', 'python3');
-
-  // miniconda접근 환경
-  // const pythonPath = path.join(
-  //   '/home/ubuntu/miniconda',
-  //   'envs',
-  //   'myenv',
-  //   'bin',
-  //   'python3'
-  // );
-
   const result = spawn(pythonExePath, [scriptPath, ...args]);
 
   let responseData = '';
 
-  // 누적해서 값을 받음
+  // stdout으로 데이터를 누적해서 받음
   result.stdout.on('data', function (data) {
     responseData += data.toString();
   });
 
-  // 데이터가 올 때 0 (성공), responseData를 json 형식으로 변환
+  // 스크립트 실행 완료 후 결과 처리
   result.on('close', (code) => {
     if (code === 0) {
       try {
         const jsonResponse = JSON.parse(responseData);
         res.status(200).json(jsonResponse);
       } catch (e) {
-        res.status(500).json({ error: 'Failed to parse JSON response' });
+        res.status(500).json({ error: 'JSON 응답 파싱에 실패했습니다.' });
       }
     } else {
-      res.status(500).json({ error: `Child Process exited with code ${code}` });
+      res
+        .status(500)
+        .json({ error: `프로세스가 코드 ${code}로 종료되었습니다.` });
     }
   });
 
-  // res error (json으로 변환되지 않으면 error)
+  // 오류 메시지 처리
   result.stderr.on('data', (data) => {
     console.log(`stderr: ${data}`);
   });
+
+  // 입력 데이터가 있을 경우 stdin으로 전달
+  if (inputData) {
+    result.stdin.write(JSON.stringify(inputData));
+    result.stdin.end(); // 입력 데이터 전송 완료
+  }
 }
 
 // 라우트 정의, 각 라우트에서 반복된 부분을 함수 호출로 대체
@@ -96,6 +84,11 @@ app.get('/genres/:genre/:count', (req, res) => {
 app.get('/item-based/:item', (req, res) => {
   const item = req.params.item;
   executePythonScript('recommender.py', ['item-based', item], res);
+});
+
+app.post('/user-based', (req, res) => {
+  const inputRatingDict = req.body;
+  executePythonScript('recommender.py', ['user-based'], res, inputRatingDict);
 });
 
 // 서버 시작
